c75f31956ab08be2359f9dfc122944cee2967844
Rajput-Suraj <surajarajput@gmail.com>
2023-10-07 19:05:51 +0530

    Conditionally rendered sold-out pizza

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,6 +7,10 @@ const Menu = () => {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all
+        organic, all delicious.
+      </p>
       <ul className="pizzas">
         {pizzaData.map((pizza) => (
           <Pizza pizzaObj={pizza} key={pizza.name} />
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,14 +1,14 @@
 import '../index.css';
 
 const Pizza = (props) => {
-  const { name, ingredients, photoName, price } = props.pizzaObj;
+  const { name, ingredients, photoName, price, soldOut } = props.pizzaObj;
   return (
-    <li className="pizza">
+    <li className={`pizza ${soldOut ? 'sold-out' : ''}`}>
       <img src={photoName} alt={name} />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
-        <span>{price}</span>
+        <span>{soldOut ? 'SOLD OUT' : price}</span>
       </div>
     </li>
   );
