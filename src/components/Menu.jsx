c0e5bc07caec4d259d86800f8d8845f44e83acab
Rajput-Suraj <surajarajput@gmail.com>
2023-10-07 17:34:52 +0530

    Updated pizza component to receive props

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,25 @@
 import Pizza from './Pizza';
+import Spinaci from '../assets/pizzas/spinaci.jpg';
+import Focaccia from '../assets/pizzas/focaccia.jpg';
 import '../index.css';
 
 const Menu = () => {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach and ricotta cheese"
+        photoName={Spinaci}
+        price={10}
+      />
+
+      <Pizza
+        name="Focaccia"
+        ingredients="Bread with italian olive oil and rosemary"
+        photoName={Focaccia}
+        price={6}
+      />
     </main>
   );
 };
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,11 +1,15 @@
-import Spinaci from '../assets/pizzas/spinaci.jpg';
+import '../index.css';
 
-const Pizza = () => {
+const Pizza = ({ name, ingredients, photoName, price }) => {
   return (
     <div className="pizzas">
       <div className="pizza">
-        <img src={Spinaci} alt="Spinaci Pizza" />
-        <h3>Pizza Spinaci</h3>
+        <img src={photoName} alt={name} />
+        <div>
+          <h3>{name}</h3>
+          <p>{ingredients}</p>
+          <span>{price}</span>
+        </div>
       </div>
     </div>
   );
